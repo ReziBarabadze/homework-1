4feb192394c2e169f70ba7aa2550320a47d41d90
Hamasaqalamidi <rezibarabadze153@gmail.com>
2024-07-26 17:56:22 +0400

    add html & js

---
+++ script.js
@@ -0,0 +1,31 @@
+// დავალება 1
+
+let cvladi = 100;
+
+if(cvladi < 50) {
+  console.log('ნაკლებია 50_ზე');
+} else if (cvladi > 20) {
+  console.log('მეტია 20_ზე');
+} else {
+  console.log('შეცდომა');
+}
+// დავალება 2
+
+let Name = 'მარიამ';
+
+if(Name == 'მარიამ'){
+  console.log('true');
+} else {
+  console.log('false');
+}
+
+// დავალება 3 
+
+let saxeli = 'მარიამ';
+
+if (saxeli == 'მარიამ') {
+  console.log('true');
+} else {
+  console.log('false');
+}
+// ბოლო დავალება ვერგავიგე :/
